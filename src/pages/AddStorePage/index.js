b908b6d0c0e08cea625be66a4bc6cd55430e8456
Bryan Lim <bryanjlim@outlook.com>
2020-08-15 22:51:38 -0700

    Add store image

--- src/pages/AddStorePage/index.js
+++ src/pages/AddStorePage/index.js
@@ -4,6 +4,8 @@ import {
   Typography,
   TextField,
   CssBaseline,
+  Grid,
+  Card,
   Button,
 } from '@material-ui/core';
 import { withSnackbar } from 'notistack';
@@ -11,10 +13,14 @@ import BackendHelpers from '../../utils/BackendHelpers';
 
 const styles = () => {
   return {
+    root: {
+      flexGrow: 1,
+      paddingTop: '25%',
+    },
     page: {
       marginLeft: 'auto',
       marginRight: 'auto',
-      maxWidth: 500,
+      maxWidth: 1000,
       width: '95%',
     },
     textField: {
@@ -58,6 +64,7 @@ class AddStorePage extends React.Component {
       emailError: false,
       addressError: false,
       addButton: false,
+      initialLoad: true,
     };
   }
 
@@ -69,73 +76,97 @@ class AddStorePage extends React.Component {
         <CssBaseline />
 
         {/* ADD NEW STORE SECTION */}
-        <div>
-          <Typography variant="h4">
-            <b>Add Store Page</b>
-          </Typography>
-          <div className={classes.fieldItem}>
-            <Typography variant="h6">
-              <b>Store Name</b>
-            </Typography>
-            <TextField
-              error={!this.state.storeName}
-              className={classes.textField}
-              variant="outlined"
-              value={this.state.storeName}
-              onChange={(evt) => this.setState({ storeName: evt.target.value })}
-            />
-          </div>
-          <div className={classes.fieldItem}>
-            <Typography variant="h6">
-              <b>Authorized Employee Emails</b>
-            </Typography>
-            <TextField
-              error={!this.state.authorizedEmployees}
-              className={classes.textField}
-              variant="outlined"
-              multiline
-              rows="5"
-              value={this.state.authorizedEmployees}
-              onChange={(evt) =>
-                this.setState({ authorizedEmployees: evt.target.value })
-              }
-            />
-          </div>
-          <div className={classes.fieldItem}>
-            <Typography variant="h6">
-              <b>Address</b>
-            </Typography>
-            <TextField
-              error={!this.state.address}
-              className={classes.textField}
-              variant="outlined"
-              value={this.state.address}
-              onChange={(evt) => this.setState({ address: evt.target.value })}
-            />
-          </div>
-          <div>
-            <Button
-              className={classes.addButton}
-              variant="contained"
-              onClick={() => {
-                if (
-                  this.state.storeName &&
-                  this.state.authorizedEmployees &&
-                  this.state.address
-                ) {
-                  this.props.enqueueSnackbar('Successfully added store.');
-                  BackendHelpers.addStore(this.state.storeName, {
-                    authorizedEmployees: this.state.authorizedEmployees.split(
-                      ','
-                    ),
-                    address: this.state.address,
-                  });
-                }
-              }}
-            >
-              Add Store
-            </Button>
-          </div>
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <div>
+                <img
+                  style={{ maxWidth: '100%' }}
+                  src={require('../../images/store.png')}
+                  alt="A minamalist store icon"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <div style={{ marginLeft: '3em' }}>
+                <Typography variant="h4">
+                  <b>Add Your Store</b>
+                </Typography>
+                <div className={classes.fieldItem}>
+                  <Typography variant="h6">
+                    <b>Store Name</b>
+                  </Typography>
+                  <TextField
+                    error={!this.state.storeName && !this.state.initialLoad}
+                    className={classes.textField}
+                    variant="outlined"
+                    value={this.state.storeName}
+                    onChange={(evt) =>
+                      this.setState({ storeName: evt.target.value })
+                    }
+                  />
+                </div>
+                <div className={classes.fieldItem}>
+                  <Typography variant="h6">
+                    <b>Authorized Employee Emails</b>
+                  </Typography>
+                  <TextField
+                    error={
+                      !this.state.authorizedEmployees && !this.state.initialLoad
+                    }
+                    className={classes.textField}
+                    variant="outlined"
+                    multiline
+                    rows="5"
+                    value={this.state.authorizedEmployees}
+                    onChange={(evt) =>
+                      this.setState({
+                        authorizedEmployees: evt.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <div className={classes.fieldItem}>
+                  <Typography variant="h6">
+                    <b>Address</b>
+                  </Typography>
+                  <TextField
+                    error={!this.state.address && !this.state.initialLoad}
+                    className={classes.textField}
+                    variant="outlined"
+                    value={this.state.address}
+                    onChange={(evt) =>
+                      this.setState({ address: evt.target.value })
+                    }
+                  />
+                </div>
+                <div>
+                  <Button
+                    className={classes.addButton}
+                    variant="contained"
+                    onClick={() => {
+                      if (
+                        this.state.storeName &&
+                        this.state.authorizedEmployees &&
+                        this.state.address
+                      ) {
+                        this.props.enqueueSnackbar('Successfully added store.');
+                        BackendHelpers.addStore(this.state.storeName, {
+                          authorizedEmployees: this.state.authorizedEmployees.split(
+                            ','
+                          ),
+                          address: this.state.address,
+                        });
+                      }
+                      this.setState({ initialLoad: false });
+                    }}
+                  >
+                    Add Store
+                  </Button>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
         </div>
       </div>
     );
